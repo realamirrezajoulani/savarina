764a9d29b74b8612e62aee2740ee6ae89c383e68
Amirreza <realamirrezajoulani@gmail.com>
2025-04-10 12:01:51 +0430

    update admin access level to default

--- back-end/src/routers/admin.py
+++ back-end/src/routers/admin.py
@@ -49,15 +49,15 @@ async def create_admin(
         *,
         session: AsyncSession = Depends(get_session),
         admin_create: AdminCreate,
-        # _user: dict = Depends(
-        #     require_roles(
-        #         AdminRole.SUPER_ADMIN.value,
-        #         AdminRole.GENERAL_ADMIN.value,
-        #     )
-        # ),
-        # _token: str = Depends(oauth2_scheme),
+        _user: dict = Depends(
+            require_roles(
+                AdminRole.SUPER_ADMIN.value,
+                AdminRole.GENERAL_ADMIN.value,
+            )
+        ),
+        _token: str = Depends(oauth2_scheme),
 ):
-    # final_role = AdminRole.GENERAL_ADMIN.value if _user["role"] == AdminRole.GENERAL_ADMIN.value else admin_create.role
+    final_role = AdminRole.GENERAL_ADMIN.value if _user["role"] == AdminRole.GENERAL_ADMIN.value else admin_create.role
 
     hashed_password = get_password_hash(admin_create.password)
 
